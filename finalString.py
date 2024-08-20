6c703f9193b00855a88dcba31168f7d509c641dc
Tsedeke <164669795+TsedekeT@users.noreply.github.com>
2024-08-20 15:15:26 +0300

    Create finalString.py

---
+++ finalString.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        result = ""
+        for i in s:
+            if i == 'i':
+                result = result[ ::-1]
+            else:
+                result += i    
+        return result
